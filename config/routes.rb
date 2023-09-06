7b5009129dc3398d684a143509cc28da9111dca4
文静 <ruhuiwen0608@gmail.com>
2023-09-06 22:49:12 +0300

    add bootstrap css & javascript

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ('/')
   # root 'articles#index'
+  root to: 'main#index'
+  
   # GET /about
   get 'about', to: 'about#index'
 
-  root to: 'main#index'
 
 end
