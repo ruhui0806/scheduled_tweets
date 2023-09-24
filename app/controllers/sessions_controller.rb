efce4642f93eb45a6a738aa720157028e6e7f1b8
文静 <ruhuiwen0608@gmail.com>
2023-09-24 15:29:19 +0300

    add log-in for users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password]) # comes from the "has_secure_password" in the user model, to check if the password is valid for the user
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Logged in successfully'
+    else
+      flash[:alert] = 'Invalid email or password'
+      render :new
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: 'Logged out!'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-
   # Defines the root path route ('/')
   # root 'articles#index'
   root to: 'main#index'
@@ -8,8 +7,11 @@ Rails.application.routes.draw do
   # GET /about
   get 'about-us', to: 'about#index', as: :about
 
-  # POST /sign_up
   get 'sign-up', to: 'registrations#new'
   post 'sign-up', to: 'registrations#create'
+
+  get 'sign-in', to: 'sessions#new' # display the sign-in form
+  post 'sign-in', to: 'sessions#create' #save the new session
+
   delete 'logout', to: 'sessions#destroy'
 end
