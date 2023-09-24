5fa332b86b5b5d43242a292a471bea9b86be1001
文静 <ruhuiwen0608@gmail.com>
2023-09-24 14:34:31 +0300

    update session coockie with find_by instead of find, so that it wont throw an error when no user was find

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,7 @@ class MainController < ApplicationController
   # end
   def index
     if session[:user_id]
-      @user = User.find(session[:user_id])
+      @user = User.find_by(id: session[:user_id])
     end
   end
 end
