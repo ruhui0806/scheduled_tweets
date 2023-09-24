11eeb9d2b548ddb8b400616498154e313e7384ce
文静 <ruhuiwen0608@gmail.com>
2023-09-24 14:33:14 +0300

    login in with sesstion cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,11 @@
 class MainController < ApplicationController
+  # def index
+  #   flash.now[:notice] = "Logged in successfully"
+  #   flash.now[:alert] = "Invalid email or password"
+  # end
   def index
-    flash.now[:notice] = "Logged in successfully"
-    flash.now[:alert] = "Invalid email or password"
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
   end
 end
